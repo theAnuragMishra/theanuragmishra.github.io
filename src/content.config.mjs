2c145aa47b1e8a446b14d51d8a1129b0f3152964
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-10-22 14:59:48 +0530

    create main page

---
+++ src/content.config.mjs
@@ -0,0 +1,22 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date().optional(),
+    description: z.string().optional(),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    topics: z.array(z.string()),
+  }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog };
