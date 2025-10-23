e1ee2ac889b42fa8d23d3bec18fa7b0bfaf1df63
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2025-10-23 19:33:00 +0530

    add remark plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 import expressiveCode from "astro-expressive-code";
+import remarkDirective from "remark-directive";
+import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +14,7 @@ export default defineConfig({
   },
   site: "https://anurag-mishra.netlify.app",
   integrations: [sitemap(), expressiveCode()],
+  markdown: {
+    remarkPlugins: [remarkDirective, remarkGithubAdmonitionsToDirectives],
+  },
 });
